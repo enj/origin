add3b92446d38c083d49dc82544ded3ff9ab7fdc
enj <mkhan@redhat.com>
2016-07-21 00:04:27 -0400

    use env vars in kerb configured

--- test/extended/testdata/gssapi/fedora/kerberos_configured/configure.sh
+++ test/extended/testdata/gssapi/fedora/kerberos_configured/configure.sh
@@ -1,5 +1,2 @@
-export host='gssapiproxy-server.gssapiproxy.svc.cluster.local'
-export realm=`echo ${host} | tr [a-z] [A-Z]`
-
-sed -i.bak1 -e "s/EXAMPLE\.COM/$realm/g" /etc/krb5.conf
-sed -i.bak2 -e "s/example\.com/$host/g" /etc/krb5.conf
+sed -i.bak1 -e "s/EXAMPLE\.COM/${REALM}/g" /etc/krb5.conf
+sed -i.bak2 -e "s/example\.com/${HOST}/g" /etc/krb5.conf
