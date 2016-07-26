05d52bfc8ebc19d647bfdba25b9d27e0db913dd1
enj <mkhan@redhat.com>
2016-07-26 12:57:05 -0400

    lots of success functions

--- test/extended/gssapi.sh
+++ test/extended/gssapi.sh
@@ -6,6 +6,11 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+STARTTIME="$(date +%s)"
+
+PROJECT='gssapiproxy'
+TEST_NAME="test-extended/${PROJECT}"
+
 OS_ROOT="$(dirname "${BASH_SOURCE}")/../.."
 cd "${OS_ROOT}"
 source hack/lib/init.sh
@@ -13,7 +18,7 @@ source hack/lib/init.sh
 os::build::setup_env
 
 os::util::environment::setup_time_vars
-os::util::environment::setup_all_server_vars "test-extended/gssapiproxy"
+os::util::environment::setup_all_server_vars "${TEST_NAME}"
 os::util::environment::use_sudo
 
 os::log::stacktrace::install
@@ -22,46 +27,80 @@ os::log::start_system_logger
 ensure_iptables_or_die ## TODO Is this needed?
 
 reset_tmp_dir
-JUNIT_REPORT_OUTPUT="${LOG_DIR}/raw_test_output.log"
-JUNIT_GSSAPI_OUTPUT="${LOG_DIR}/raw_test_output_gssapi.log"
 
-os::test::junit::declare_suite_start "test-extended/gssapiproxy"
+# Allow setting $JUNIT_REPORT to toggle output behavior
+if [[ -n "${JUNIT_REPORT:-}" ]]; then
+    export JUNIT_REPORT_OUTPUT="${LOG_DIR}/raw_test_output.log"
+fi
 
-hack/build-go.sh cmd/oc -tags=gssapi
+# Always keep containers' raw output for simplicity
+export JUNIT_GSSAPI_OUTPUT="${LOG_DIR}/raw_test_output_gssapi.log"
+
+os::test::junit::declare_suite_start "${TEST_NAME}"
+
+os::cmd::expect_success 'hack/build-go.sh cmd/oc -tags=gssapi'
 os::cmd::expect_success_and_text 'oc version' 'GSSAPI Kerberos SPNEGO'
 
 function cleanup() {
     out=$?
-    os::test::junit::reconcile_output
+    set +e
     cleanup_openshift
 
-    # use the junitreport tool to generate us a report
-    "${OS_ROOT}/hack/build-go.sh" tools/junitreport
-    junitreport="$(os::build::find-binary junitreport)"
+    # TODO(skuznets): un-hack this nonsense once traps are in a better state
+    if [[ -n "${JUNIT_REPORT_OUTPUT:-}" ]]; then
+      # get the jUnit output file into a workable state in case we crashed in the middle of testing something
+      os::test::junit::reconcile_output
 
-    cat "${JUNIT_REPORT_OUTPUT}" "${JUNIT_GSSAPI_OUTPUT}"   \
-    | "${junitreport}"  --type oscmd                        \
-                        --suites nested                     \
-                        --roots github.com/openshift/origin \
-                        --output "${ARTIFACT_DIR}/report.xml"
-    cat "${ARTIFACT_DIR}/report.xml" | "${junitreport}" summarize
+      # check that we didn't mangle jUnit output
+      os::test::junit::check_test_counters
 
-    echo "[INFO] Exiting"
-    [[ -n "${SKIP_TEARDOWN-}" ]] && oc project gssapiproxy ##TODO remove
-    return $out
+      # use the junitreport tool to generate us a report
+      "${OS_ROOT}/hack/build-go.sh" tools/junitreport
+      junitreport="$(os::build::find-binary junitreport)"
+
+      if [[ -z "${junitreport}" ]]; then
+          echo "It looks as if you don't have a compiled junitreport binary"
+          echo
+          echo "If you are running from a clone of the git repo, please run"
+          echo "'./hack/build-go.sh tools/junitreport'."
+          exit 1
+      fi
+
+      cat "${JUNIT_REPORT_OUTPUT}" "${JUNIT_GSSAPI_OUTPUT}"    \
+        | "${junitreport}" --type oscmd                        \
+                           --suites nested                     \
+                           --roots github.com/openshift/origin \
+                           --output "${ARTIFACT_DIR}/report.xml"
+      cat "${ARTIFACT_DIR}/report.xml" | "${junitreport}" summarize
+    fi
+
+    ENDTIME=$(date +%s); echo "$0 took $(($ENDTIME - $STARTTIME)) seconds"
+    exit $out
 }
 
-function wait_for_auth_proxy() {
+function update_auth_proxy_config {
     local server_config="${1}"
-    local spec='{.items[0].spec.containers[0].env[?(@.name=="SERVER")].value}_{.items[0].status.conditions[?(@.type=="Ready")].status}'
-    os::cmd::try_until_text "oc get pods -l deploymentconfig=gssapiproxy-server -o jsonpath='${spec}'" "^${server_config}_True$"
+    local spec='{.items[0].spec.containers[0].env[?(@.name=="SERVER")].value}'
+    spec+='_'
+    spec+='{.items[0].spec.containers[0].env[?(@.name=="SERVER_GSSAPI_BASIC_AUTH")].value}'
+    spec+='_'
+    spec+='{.items[0].status.conditions[?(@.type=="Ready")].status}'
+
+    if [[ "${server_config}" = 'SERVER_GSSAPI_BASIC_FALLBACK' ]]; then
+        local SERVER_GSSAPI_BASIC_AUTH=on
+    else
+        local SERVER_GSSAPI_BASIC_AUTH=off
+    fi
+
+    os::cmd::expect_success "oc set env dc/gssapiproxy-server SERVER='${server_config}' SERVER_GSSAPI_BASIC_AUTH='${SERVER_GSSAPI_BASIC_AUTH}'"
+    os::cmd::try_until_text "oc get pods -l deploymentconfig=gssapiproxy-server -o jsonpath='${spec}'" "^${server_config}_${SERVER_GSSAPI_BASIC_AUTH}_True$"
 }
 
 function run_gssapi_tests() {
     local image_name="${1}"
     local server_config="${2}"
     oc run "${image_name}" \
-        --image="gssapiproxy/${image_name}" \
+        --image="${PROJECT}/${image_name}" \
         --generator=run-pod/v1 --restart=Never --attach \
         --env=SERVER="${server_config}" \
         1> "${LOG_DIR}/${image_name}-${server_config}.log" \
@@ -106,9 +145,9 @@ wait_for_registry
 REGISTRY_IP="$(oc get svc docker-registry -n default -o jsonpath='{.spec.clusterIP}:{.spec.ports[0].targetPort}')"
 # TODO REGISTRY_IP re-add
 
-oc login -u system:admin
-oc new-project gssapiproxy
-oadm policy add-scc-to-user anyuid -z default -n gssapiproxy
+os::cmd::expect_success 'oc login -u system:admin'
+os::cmd::expect_success "oc new-project ${PROJECT}"
+os::cmd::expect_success "oadm policy add-scc-to-user anyuid -z default -n ${PROJECT}"
 
 # create all the resources we need
 cp -R test/extended/testdata/gssapi "${BASETMPDIR}"
@@ -116,13 +155,13 @@ TEST_DATA="${BASETMPDIR}/gssapi"
 
 HOST='gssapiproxy-server.gssapiproxy.svc.cluster.local'
 REALM="${HOST^^}"
-BACKEND='https://openshift.default.svc.cluster.local'
+BACKEND='https://openshift.default.svc.cluster.local:443'
 
-oc create -f "${TEST_DATA}/proxy"
+os::cmd::expect_success "oc create -f '${TEST_DATA}/proxy'"
 
 # kick off a build and wait for it to finish
-oc set env dc/gssapiproxy-server HOST="${HOST}" REALM="${REALM}" BACKEND="${BACKEND}"
-oc start-build --from-dir="${TEST_DATA}/proxy" --follow gssapiproxy
+os::cmd::expect_success "oc set env dc/gssapiproxy-server HOST='${HOST}' REALM='${REALM}' BACKEND='${BACKEND}'"
+os::cmd::expect_success "oc start-build --from-dir='${TEST_DATA}/proxy' --follow gssapiproxy"
 
 OS_IMAGES=(fedora ubuntu)
 
@@ -134,27 +173,27 @@ for os_image in "${OS_IMAGES[@]}"; do
         cp ../scripts/test-wrapper.sh base
         cp ../scripts/gssapi-tests.sh base
 
-        oc create -f base
-        oc create -f kerberos
-        oc create -f kerberos_configured
+        os::cmd::expect_success 'oc create -f base'
+        os::cmd::expect_success 'oc create -f kerberos'
+        os::cmd::expect_success 'oc create -f kerberos_configured'
     popd
 
     # TODO Figure out how to set environment variables with binary builds; needed for ${REALM} and ${HOST}
 
     pushd "${TEST_DATA}/${os_image}"
-        # oc start-build --from-dir=base --follow "${os_image}-gssapi-base"
+        # os::cmd::expect_success "oc start-build --from-dir=base --follow '${os_image}-gssapi-base'"
         pushd base
-            docker build --build-arg REALM="${REALM}" --build-arg HOST="${HOST}" -t "gssapiproxy/${os_image}-gssapi-base:latest" .
+            os::cmd::expect_success "docker build --build-arg REALM='${REALM}' --build-arg HOST='${HOST}' -t '${PROJECT}/${os_image}-gssapi-base:latest' ."
         popd
 
-        # oc start-build --from-dir=kerberos --follow "${os_image}-gssapi-kerberos"
+        # os::cmd::expect_success "oc start-build --from-dir=kerberos --follow '${os_image}-gssapi-kerberos'"
         pushd kerberos
-            docker build -t "gssapiproxy/${os_image}-gssapi-kerberos:latest" .
+            os::cmd::expect_success "docker build -t '${PROJECT}/${os_image}-gssapi-kerberos:latest' ."
         popd
 
-        # oc start-build --from-dir=kerberos_configured --follow "${os_image}-gssapi-kerberos-configured"
+        # os::cmd::expect_success "oc start-build --from-dir=kerberos_configured --follow '${os_image}-gssapi-kerberos-configured'"
         pushd kerberos_configured
-            docker build -t "gssapiproxy/${os_image}-gssapi-kerberos-configured:latest" .
+            os::cmd::expect_success "docker build -t '${PROJECT}/${os_image}-gssapi-kerberos-configured:latest' ."
         popd
     popd
 
@@ -162,8 +201,7 @@ done
 
 for server_config in SERVER_GSSAPI_ONLY SERVER_GSSAPI_BASIC_FALLBACK; do
 
-    oc set env dc/gssapiproxy-server SERVER="${server_config}"
-    wait_for_auth_proxy "${server_config}"
+    update_auth_proxy_config "${server_config}"
 
     for os_image in "${OS_IMAGES[@]}"; do
 
