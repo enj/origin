197e2549b053a7b72eafe55fdb121234758643af
Monis Khan <mkhan@redhat.com>
2017-11-29 20:33:58 -0500

    Make client authorizations reentrant to UID change
    This change makes it so that if an OAuthClientAuthorization
    references a user with the wrong UID, that client authorization is
    deleted.  This UID mismatch is caused by the deletion and recreation
    of users.  Thus this makes it so that the "new" user is unaffected
    by the "old" user's client authorization.  The new user simply goes
    through the grant flow just as if no client authorization existed.
    
    Signed-off-by: Monis Khan <mkhan@redhat.com>

--- pkg/auth/oauth/registry/grantchecker.go
+++ pkg/auth/oauth/registry/grantchecker.go
@@ -1,8 +1,6 @@
 package registry
 
 import (
-	"fmt"
-
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apiserver/pkg/authentication/user"
@@ -11,6 +9,8 @@ import (
 	oauthclient "github.com/openshift/origin/pkg/oauth/generated/internalclientset/typed/oauth/internalversion"
 	"github.com/openshift/origin/pkg/oauth/registry/oauthclientauthorization"
 	"github.com/openshift/origin/pkg/oauth/scope"
+
+	"github.com/golang/glog"
 )
 
 type ClientAuthorizationGrantChecker struct {
@@ -30,12 +30,22 @@ func (c *ClientAuthorizationGrantChecker) HasAuthorizedClient(user user.Info, gr
 	if err != nil {
 		return false, err
 	}
-	if len(authorization.UserUID) != 0 && authorization.UserUID != user.GetUID() {
-		return false, fmt.Errorf("user %s UID %s does not match stored client authorization value for UID %s", user.GetName(), user.GetUID(), authorization.UserUID)
+
+	// Validation prevents authorization.UserUID from being empty (and always has).
+	// However, user.GetUID() is empty during impersonation, meaning this flow does not work for impersonation.
+	// This is fine because no OAuth / grant flow works with impersonation in general.
+	if user.GetUID() != authorization.UserUID {
+		glog.Infof("%#v does not match stored client authorization %#v, attempting to delete stale authorization", user, authorization)
+		if err := c.client.Delete(id, nil); err != nil && !errors.IsNotFound(err) {
+			return false, err
+		}
+		return false, nil
 	}
+
 	// TODO: improve this to allow the scope implementation to determine overlap
 	if !scope.Covers(authorization.Scopes, scope.Split(grant.Scope)) {
 		return false, nil
 	}
+
 	return true, nil
 }
