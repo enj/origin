a5bfeee22429ea3236d00219054484e87b0d63d4
Monis Khan <mkhan@redhat.com>
2018-01-11 16:26:29 -0500

    wip2
    Signed-off-by: Monis Khan <mkhan@redhat.com>

--- pkg/cmd/util/serviceability/ratelimiting.go
+++ pkg/cmd/util/serviceability/ratelimiting.go
@@ -1,6 +1,8 @@
 package serviceability
 
 import (
+	"runtime/debug"
+	"strings"
 	"sync"
 
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
@@ -9,7 +11,13 @@ import (
 	"github.com/golang/groupcache/lru"
 )
 
-const cacheSize = 1000
+const (
+	cacheSize = 1000
+	// logError in utilruntime has a dept of 2, which does not work with our extra indirection
+	// we add 1 for rateLimitedErrorHandler.handleErr
+	// and we add 1 more for its pointer that is stored in utilruntime.ErrorHandlers
+	depth = 4
+)
 
 type rateLimitedErrorHandler struct {
 	handlers []func(error)
@@ -17,10 +25,11 @@ type rateLimitedErrorHandler struct {
 	mutex    sync.Mutex
 }
 
-func (r *rateLimitedErrorHandler) handle(err error) {
+func (r *rateLimitedErrorHandler) handleErr(err error) {
 	r.mutex.Lock()
 	defer r.mutex.Unlock()
-	key := errKey{caller: utilruntime.GetCaller(), errorMessage: err.Error()}
+	stack := strings.Join(strings.Split(string(debug.Stack()), "\n")[depth*2:], "\n")
+	key := errKey{stack: stack, errorMessage: err.Error()}
 	// only call r.handlers if this key is not in the cache
 	if _, ok := r.cache.Get(key); !ok {
 		r.cache.Add(key, err)
@@ -31,7 +40,7 @@ func (r *rateLimitedErrorHandler) handle(err error) {
 }
 
 type errKey struct {
-	caller, errorMessage string
+	stack, errorMessage string
 }
 
 func BehaviorOnPanicWithRateLimitedErrors(mode string) func() {
@@ -42,19 +51,18 @@ func BehaviorOnPanicWithRateLimitedErrors(mode string) func() {
 	// save a copy
 	origErrorHandlers := utilruntime.ErrorHandlers
 	// override with our rate limited error handler that throttles calls to origErrorHandlers
-	utilruntime.ErrorHandlers = []func(error){newRateLimitedErrorHandler(origErrorHandlers)}
+	utilruntime.ErrorHandlers = []func(error){newRateLimitedErrorHandler(origErrorHandlers).handleErr}
 	// return BehaviorOnPanic defer func so we can be used to replace it easily
 	return fn
 }
 
-func newRateLimitedErrorHandler(funcs []func(error)) func(error) {
+func newRateLimitedErrorHandler(funcs []func(error)) *rateLimitedErrorHandler {
 	// create a slice with our error logger and the given funcs
 	handlers := append([]func(error){logError}, funcs...)
 	// wrap the slice with our rate limiter
-	errorHandler := &rateLimitedErrorHandler{handlers: handlers, cache: lru.New(cacheSize)}
-	return errorHandler.handle
+	return &rateLimitedErrorHandler{handlers: handlers, cache: lru.New(cacheSize)}
 }
 
 func logError(err error) {
-	glog.ErrorDepth(3, err)
+	glog.ErrorDepth(depth, err)
 }
