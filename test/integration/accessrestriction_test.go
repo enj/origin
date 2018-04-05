ec1870d25c79a2b5c47ffb95cde607bfa4efa3e9
Monis Khan <mkhan@redhat.com>
2018-04-05 11:52:57 -0400

    fixup! access restriction authorizer

--- test/integration/accessrestriction_test.go
+++ test/integration/accessrestriction_test.go
@@ -30,6 +30,7 @@ func TestAccessRestrictionEscalationCheck(t *testing.T) {
 		t.Fatal(err)
 	}
 	rbacClient := rbacinternalversion.NewForConfigOrDie(clusterAdminClientConfig)
+	clusterAdminAccessRestrictionClient := authorizationv1alpha1clientset.NewForConfigOrDie(clusterAdminClientConfig).AccessRestrictions()
 
 	clusterRoleName := "almost-cluster-admin" // can do everything except URLs so still not enough for escalation check
 	user := "mo"
@@ -52,16 +53,16 @@ func TestAccessRestrictionEscalationCheck(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+	userAccessRestrictionClient := authorizationv1alpha1clientset.NewForConfigOrDie(userConfig).AccessRestrictions()
 
-	accessRestrictionClient := authorizationv1alpha1clientset.NewForConfigOrDie(userConfig).AccessRestrictions()
-
+	// wait for rbac to catch up
 	if err := wait.ExponentialBackoff(
 		wait.Backoff{
 			Steps:    30,
 			Duration: time.Second,
 		},
 		func() (done bool, err error) {
-			if _, err := accessRestrictionClient.List(metav1.ListOptions{}); err != nil {
+			if _, err := userAccessRestrictionClient.List(metav1.ListOptions{}); err != nil {
 				if errors.IsForbidden(err) {
 					return false, nil
 				}
@@ -72,7 +73,7 @@ func TestAccessRestrictionEscalationCheck(t *testing.T) {
 		t.Fatalf("failed to list access restriction as user: %#v", err)
 	}
 
-	_, err = accessRestrictionClient.Create(&authorizationv1alpha1.AccessRestriction{
+	accessRestriction := &authorizationv1alpha1.AccessRestriction{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: "does-not-matter",
 		},
@@ -92,11 +93,49 @@ func TestAccessRestrictionEscalationCheck(t *testing.T) {
 				},
 			},
 		},
-	})
+	}
+
+	_, err = userAccessRestrictionClient.Create(accessRestriction)
 	if err == nil {
 		t.Fatal("expected non-nil create error for access restriction")
 	}
 	if !errors.IsForbidden(err) || !strings.Contains(err.Error(), "must have cluster-admin privileges to write access restrictions") {
 		t.Fatalf("expected forbidden error for access restrction create: %#v", err)
+	}
+
+	if accessRestriction, err = clusterAdminAccessRestrictionClient.Create(accessRestriction); err != nil {
+		t.Fatalf("unexpected error for access restrction create as system:masters: %#v", err)
+	}
+
+	// delete the permissions of all users so only system:masters can do anything
+	if err := rbacClient.ClusterRoleBindings().DeleteCollection(nil, metav1.ListOptions{}); err != nil {
+		t.Fatal(err)
+	}
+
+	// wait for rbac to catch up
+	if err := wait.ExponentialBackoff(
+		wait.Backoff{
+			Steps:    30,
+			Duration: time.Second,
+		},
+		func() (done bool, err error) {
+			_, err = userAccessRestrictionClient.List(metav1.ListOptions{})
+			if errors.IsForbidden(err) {
+				return true, nil
+			}
+			return false, err
+		}); err != nil {
+		t.Fatalf("failed to revoke right for user: %#v", err)
+	}
+
+	// make sure system:masters can still pass the escalation check even with no RBAC rules
+	expectedDeniedUser := "other-user"
+	accessRestriction.Spec.DeniedSubjects[0].UserRestriction.Users[0] = expectedDeniedUser
+	if updatedAccessRestriction, err := clusterAdminAccessRestrictionClient.Update(accessRestriction); err != nil {
+		t.Fatalf("failed to update access restriction as system:masters: %#v", err)
+	} else {
+		if actualDeniedUser := updatedAccessRestriction.Spec.DeniedSubjects[0].UserRestriction.Users[0]; expectedDeniedUser != actualDeniedUser {
+			t.Fatalf("updated access restriction does not match, expected %s, actual %s", expectedDeniedUser, actualDeniedUser)
+		}
 	}
 }
