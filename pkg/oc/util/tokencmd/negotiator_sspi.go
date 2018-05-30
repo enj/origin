f4675a75fe24b7cfe7c263f6dd7a21bbfa8629e9
Monis Khan <mkhan@redhat.com>
2018-05-30 16:34:16 -0400

    wip001
    Signed-off-by: Monis Khan <mkhan@redhat.com>

--- pkg/oc/util/tokencmd/negotiator_sspi.go
+++ pkg/oc/util/tokencmd/negotiator_sspi.go
@@ -3,9 +3,7 @@
 package tokencmd
 
 import (
-	"errors"
-	"os"
-	"os/user"
+	"fmt"
 	"strings"
 
 	utilerrors "k8s.io/apimachinery/pkg/util/errors"
@@ -19,7 +17,7 @@ func SSPIEnabled() bool {
 	return true
 }
 
-type sspiNegotiater struct {
+type sspiNegotiator struct {
 	principalName string
 	password      string
 
@@ -28,16 +26,16 @@ type sspiNegotiater struct {
 	complete bool
 }
 
-func NewSSPINegotiator(principalName, password string) Negotiater {
-	return &sspiNegotiater{principalName: principalName, password: password}
+func NewSSPINegotiator(principalName, password string) Negotiator {
+	return &sspiNegotiator{principalName: principalName, password: password}
 }
 
-func (s *sspiNegotiater) Load() error {
+func (s *sspiNegotiator) Load() error {
 	glog.V(5).Info("Attempt to load SSPI")
 	return nil
 }
 
-func (s *sspiNegotiater) InitSecContext(requestURL string, challengeToken []byte) ([]byte, error) {
+func (s *sspiNegotiator) InitSecContext(requestURL string, challengeToken []byte) ([]byte, error) {
 	if s.cred == nil || s.ctx == nil {
 		cred, err := s.getUserCredentials()
 		if err != nil {
@@ -45,6 +43,7 @@ func (s *sspiNegotiater) InitSecContext(requestURL string, challengeToken []byte
 			return nil, err
 		}
 		s.cred = cred
+		glog.V(5).Info("getUserCredentials successful")
 
 		serviceName, err := getServiceName('/', requestURL)
 		if err != nil {
@@ -52,29 +51,30 @@ func (s *sspiNegotiater) InitSecContext(requestURL string, challengeToken []byte
 		}
 
 		glog.V(5).Infof("importing service name %s", serviceName)
-		ctx, token, err := negotiate.NewClientContext(s.cred, serviceName)
+		ctx, outputToken, err := negotiate.NewClientContext(s.cred, serviceName)
 		if err != nil {
 			glog.V(5).Infof("NewClientContext returned error: %v", err)
 			return nil, err
 		}
 		s.ctx = ctx
-		return token, nil
+		glog.V(5).Info("NewClientContext successful")
+		return outputToken, nil
 	}
 
-	complete, token, err := s.ctx.Update(challengeToken)
+	complete, outputToken, err := s.ctx.Update(challengeToken)
 	if err != nil {
 		glog.V(5).Infof("Update returned error: %v", err)
 		return nil, err
 	}
 	s.complete = complete
-	return token, nil
+	return outputToken, nil
 }
 
-func (s *sspiNegotiater) IsComplete() bool {
+func (s *sspiNegotiator) IsComplete() bool {
 	return s.complete
 }
 
-func (s *sspiNegotiater) Release() error {
+func (s *sspiNegotiator) Release() error {
 	glog.V(5).Info("Attempt to release SSPI")
 	var errs []error
 	if s.ctx != nil {
@@ -93,22 +93,18 @@ func (s *sspiNegotiater) Release() error {
 	return utilerrors.NewAggregate(errs)
 }
 
-func (s *sspiNegotiater) getUserCredentials() (*sspi.Credentials, error) {
-	// Try to use principalName if possible
-	// Fallback to the current user if principalName referred to the same user or was unspecified
+func (s *sspiNegotiator) getUserCredentials() (*sspi.Credentials, error) {
+	// Try to use principalName if specified
 	if len(s.principalName) > 0 {
-		username, domain, err := s.splitDomainAndUsername()
+		domain, username, err := s.splitDomainAndUsername()
 		if err != nil {
 			return nil, err
 		}
-		glog.V(5).Info("Using AcquireUserCredentials because principalName is not empty")
+		glog.V(5).Infof("Using AcquireUserCredentials because principalName is not empty, principalName=%s, username=%s, domain=%s",
+			s.principalName, username, domain)
 		cred, err := negotiate.AcquireUserCredentials(domain, username, s.password)
 		if err != nil {
-			if u, uerr := user.Current(); uerr == nil && u.Username == username && domain == getCurrentUserDomain() {
-				glog.V(5).Info("Using AcquireCurrentUserCredentials because AcquireUserCredentials failed and principalName is probably the current user")
-				return negotiate.AcquireCurrentUserCredentials()
-			}
-			glog.V(5).Info("AcquireUserCredentials failed and not falling back to AcquireCurrentUserCredentials because principalName is not the current user")
+			glog.V(5).Infof("AcquireUserCredentials failed: %v", err)
 			return nil, err
 		}
 		glog.V(5).Info("AcquireUserCredentials successful")
@@ -118,30 +114,14 @@ func (s *sspiNegotiater) getUserCredentials() (*sspi.Credentials, error) {
 	return negotiate.AcquireCurrentUserCredentials()
 }
 
-const upnSeparator = "@"
+const domainSeparator = `\`
 
-func (s *sspiNegotiater) splitDomainAndUsername() (string, string, error) {
-	if strings.Contains(s.principalName, upnSeparator) {
-		data := strings.Split(s.principalName, upnSeparator)
-		if len(data) != 2 {
-			return "", "", errors.New("Invalid principalName: " + s.principalName)
-		}
-		username := data[0]
-		domain := data[1]
-		if len(domain) == 0 {
-			domain = getCurrentUserDomain()
-		}
-		return username, domain, nil
+func (s *sspiNegotiator) splitDomainAndUsername() (string, string, error) {
+	data := strings.Split(s.principalName, domainSeparator)
+	if len(data) != 2 {
+		return "", "", fmt.Errorf(`invalid principalName %s, must be in format DOMAIN\username`, s.principalName)
 	}
-	return s.principalName, getCurrentUserDomain(), nil
-}
-
-func getCurrentUserDomain() string {
-	for _, env := range []string{"USERDNSDOMAIN", "USERDOMAIN"} {
-		domain, ok := os.LookupEnv(env) // TODO there has to be a better way to do this => use syscall or sys/x/
-		if ok {
-			return domain
-		}
-	}
-	return ""
+	domain := data[0]
+	username := data[1]
+	return domain, username, nil
 }
