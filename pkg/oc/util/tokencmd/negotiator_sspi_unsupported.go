77f53f97f32b2c42c25e456a8395c79a48ea24dd
Monis Khan <mkhan@redhat.com>
2018-06-04 09:37:08 -0400

    new SSPI code
    Signed-off-by: Monis Khan <mkhan@redhat.com>

--- pkg/oc/util/tokencmd/negotiator_sspi_unsupported.go
+++ pkg/oc/util/tokencmd/negotiator_sspi_unsupported.go
@@ -2,10 +2,12 @@
 
 package tokencmd
 
+import "io"
+
 func SSPIEnabled() bool {
 	return false
 }
 
-func NewSSPINegotiator(string, string) Negotiator {
+func NewSSPINegotiator(string, string, string, io.Reader) Negotiator {
 	return newUnsupportedNegotiator("SSPI")
 }
