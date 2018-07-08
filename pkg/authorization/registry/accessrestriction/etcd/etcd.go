999e448e535465df1b712fcb2b4950beeeaf972d
Monis Khan <mkhan@redhat.com>
2018-07-07 20:08:19 -0400

    fixup! Add access restriction REST API

--- pkg/authorization/registry/accessrestriction/etcd/etcd.go
+++ pkg/authorization/registry/accessrestriction/etcd/etcd.go
@@ -1,17 +1,18 @@
 package etcd
 
 import (
+	"context"
 	"errors"
 
+	rbacv1 "k8s.io/api/rbac/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/apis/meta/internalversion"
 	"k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apiserver/pkg/endpoints/request"
 	"k8s.io/apiserver/pkg/registry/generic"
 	"k8s.io/apiserver/pkg/registry/generic/registry"
 	"k8s.io/apiserver/pkg/registry/rest"
-	"k8s.io/kubernetes/pkg/apis/rbac"
+	rbacv1helpers "k8s.io/kubernetes/pkg/apis/rbac/v1"
 	rbacregistry "k8s.io/kubernetes/pkg/registry/rbac"
 	rbacregistryvalidation "k8s.io/kubernetes/pkg/registry/rbac/validation"
 
@@ -42,9 +43,9 @@ var (
 
 	groupResource = authorizationapi.Resource("accessrestrictions")
 
-	fullAuthority = []rbac.PolicyRule{
-		rbac.NewRule("*").Groups("*").Resources("*").RuleOrDie(),
-		rbac.NewRule("*").URLs("*").RuleOrDie(),
+	fullAuthority = []rbacv1.PolicyRule{
+		rbacv1helpers.NewRule("*").Groups("*").Resources("*").RuleOrDie(),
+		rbacv1helpers.NewRule("*").URLs("*").RuleOrDie(),
 	}
 
 	errNotClusterAdmin = errors.New("must have cluster-admin privileges to write access restrictions")
@@ -70,7 +71,7 @@ func NewREST(optsGetter restoptions.Getter, ruleResolver rbacregistryvalidation.
 	return &REST{readStorage: store, storage: store, ruleResolver: ruleResolver}, nil
 }
 
-func (s *REST) Create(ctx request.Context, obj runtime.Object, createValidation rest.ValidateObjectFunc, includeUninitialized bool) (runtime.Object, error) {
+func (s *REST) Create(ctx context.Context, obj runtime.Object, createValidation rest.ValidateObjectFunc, includeUninitialized bool) (runtime.Object, error) {
 	if err := s.confirmFullAuthority(ctx, obj.(*authorizationapi.AccessRestriction).Name); err != nil {
 		return nil, err
 	}
@@ -78,7 +79,7 @@ func (s *REST) Create(ctx request.Context, obj runtime.Object, createValidation 
 	return s.storage.Create(ctx, obj, createValidation, includeUninitialized)
 }
 
-func (s *REST) Update(ctx request.Context, name string, objInfo rest.UpdatedObjectInfo, createValidation rest.ValidateObjectFunc, updateValidation rest.ValidateObjectUpdateFunc) (runtime.Object, bool, error) {
+func (s *REST) Update(ctx context.Context, name string, objInfo rest.UpdatedObjectInfo, createValidation rest.ValidateObjectFunc, updateValidation rest.ValidateObjectUpdateFunc) (runtime.Object, bool, error) {
 	if err := s.confirmFullAuthority(ctx, name); err != nil {
 		return nil, false, err
 	}
@@ -86,7 +87,7 @@ func (s *REST) Update(ctx request.Context, name string, objInfo rest.UpdatedObje
 	return s.storage.Update(ctx, name, objInfo, createValidation, updateValidation)
 }
 
-func (s *REST) Delete(ctx request.Context, name string, options *v1.DeleteOptions) (runtime.Object, bool, error) {
+func (s *REST) Delete(ctx context.Context, name string, options *v1.DeleteOptions) (runtime.Object, bool, error) {
 	if err := s.confirmFullAuthority(ctx, name); err != nil {
 		return nil, false, err
 	}
@@ -94,7 +95,7 @@ func (s *REST) Delete(ctx request.Context, name string, options *v1.DeleteOption
 	return s.storage.Delete(ctx, name, options)
 }
 
-func (s *REST) DeleteCollection(ctx request.Context, options *v1.DeleteOptions, listOptions *internalversion.ListOptions) (runtime.Object, error) {
+func (s *REST) DeleteCollection(ctx context.Context, options *v1.DeleteOptions, listOptions *internalversion.ListOptions) (runtime.Object, error) {
 	if err := s.confirmFullAuthority(ctx, ""); err != nil {
 		return nil, err
 	}
@@ -102,7 +103,7 @@ func (s *REST) DeleteCollection(ctx request.Context, options *v1.DeleteOptions, 
 	return s.storage.DeleteCollection(ctx, options, listOptions)
 }
 
-func (s *REST) confirmFullAuthority(ctx request.Context, name string) error {
+func (s *REST) confirmFullAuthority(ctx context.Context, name string) error {
 	if rbacregistry.EscalationAllowed(ctx) {
 		return nil
 	}
