2c89a1802808e7eb1c9c52a12f713b6a90c475c4
Monis Khan <mkhan@redhat.com>
2018-07-30 16:35:44 -0400

    add scaffolding for groups from IDPs
    Signed-off-by: Monis Khan <mkhan@redhat.com>

--- pkg/oauthserver/api/types.go
+++ pkg/oauthserver/api/types.go
@@ -18,6 +18,13 @@ const (
 	IdentityPreferredUsernameKey = "preferred_username"
 )
 
+var (
+	_ UserIdentityInfo = &DefaultUserIdentityInfo{}
+
+	_ user.Info            = &DefaultUserIdentityMetadata{}
+	_ UserIdentityMetadata = &DefaultUserIdentityMetadata{}
+)
+
 // UserIdentityInfo contains information about an identity.  Identities are distinct from users.  An authentication server of
 // some kind (like oauth for example) describes an identity.  Our system controls the users mapped to this identity.
 type UserIdentityInfo interface {
@@ -27,6 +34,9 @@ type UserIdentityInfo interface {
 	GetProviderName() string
 	// GetProviderUserName uniquely identifies this particular identity for this provider.  It is NOT guaranteed to be unique across providers
 	GetProviderUserName() string
+	// GetProviderGroups returns the groups associated with the user for this provider.
+	// It is NOT guaranteed to be stable across different authentication requests for the same provider.
+	GetProviderGroups() []string
 	// GetExtra is a map to allow providers to add additional fields that they understand
 	GetExtra() map[string]string
 }
@@ -36,6 +46,12 @@ type UserIdentityMapper interface {
 	// UserFor takes an identity, ignores the passed identity.Provider, forces the provider value to some other value and then creates the mapping.
 	// It returns the corresponding user.Info
 	UserFor(identityInfo UserIdentityInfo) (user.Info, error)
+}
+
+// UserIdentityMetadata is an extension to user.Info that contains a reference to an identity metadata object.
+// This object can be used to enrich later authentication flows with data such as the user's group membership from the identity provider.
+type UserIdentityMetadata interface {
+	GetIdentityMetadataName() string
 }
 
 type Client interface {
@@ -55,6 +71,7 @@ type Grant struct {
 type DefaultUserIdentityInfo struct {
 	ProviderName     string
 	ProviderUserName string
+	ProviderGroups   []string
 	Extra            map[string]string
 }
 
@@ -79,6 +96,10 @@ func (i *DefaultUserIdentityInfo) GetProviderUserName() string {
 	return i.ProviderUserName
 }
 
+func (i *DefaultUserIdentityInfo) GetProviderGroups() []string {
+	return i.ProviderGroups
+}
+
 func (i *DefaultUserIdentityInfo) GetExtra() map[string]string {
 	return i.Extra
 }
@@ -96,4 +117,20 @@ type ProviderInfo struct {
 // on particular clients.   This interface will make its easier to write a future cache on it
 type OAuthClientGetter interface {
 	Get(name string, options metav1.GetOptions) (*oauthapi.OAuthClient, error)
+}
+
+type DefaultUserIdentityMetadata struct {
+	user.Info
+	IdentityMetadataName string
+}
+
+func (i *DefaultUserIdentityMetadata) GetIdentityMetadataName() string {
+	return i.IdentityMetadataName
+}
+
+func NewDefaultUserIdentityMetadata(u user.Info, identityMetadataName string) *DefaultUserIdentityMetadata {
+	return &DefaultUserIdentityMetadata{
+		Info:                 u,
+		IdentityMetadataName: identityMetadataName,
+	}
 }
--- pkg/oauthserver/server/session/authenticator.go
+++ pkg/oauthserver/server/session/authenticator.go
@@ -7,6 +7,8 @@ import (
 	"time"
 
 	"k8s.io/apiserver/pkg/authentication/user"
+
+	authapi "github.com/openshift/origin/pkg/oauthserver/api"
 )
 
 const (
@@ -18,6 +20,8 @@ const (
 	// expiresKey is the string representation of expKey
 	// TODO drop in a release when mixed masters are no longer an issue
 	expiresKey = "expires"
+
+	identityMetadataNameKey = "identity.metadata.name" // TODO maybe use a smaller name to make cookie smaller?
 )
 
 type Authenticator struct {
@@ -61,6 +65,24 @@ func (a *Authenticator) AuthenticateRequest(req *http.Request) (user.Info, bool,
 		return nil, false, nil
 	}
 
+	// check if we reference an identity metadata object
+	identityMetadataName, ok, err := values.GetString(identityMetadataNameKey)
+	if err != nil {
+		return nil, false, err
+	}
+
+	// if we reference an identity metadata object, use it as the
+	// source of truth and ignore the remaining fields of the cookie
+	if ok {
+		// TODO use client to get identity metadata to fill this object
+		user := &user.DefaultInfo{
+			Name: "name", // TODO fix
+			UID:  "uid",  // TODO fix
+		}
+		return authapi.NewDefaultUserIdentityMetadata(user, identityMetadataName), true, nil
+	}
+
+	// otherwise fallback to name and UID
 	name, ok, err := values.GetString(userNameKey)
 	if !ok || err != nil {
 		return nil, false, err
@@ -80,15 +102,34 @@ func (a *Authenticator) AuthenticateRequest(req *http.Request) (user.Info, bool,
 }
 
 func (a *Authenticator) AuthenticationSucceeded(user user.Info, state string, w http.ResponseWriter, req *http.Request) (bool, error) {
-	return false, a.put(w, user.GetName(), user.GetUID(), time.Now().Add(a.maxAge).Unix())
+	// we always need to store an expiration time for the cookie
+	expires := time.Now().Add(a.maxAge).Unix()
+
+	// store name and UID to handle the case where we have no identity metadata
+	// or when we may have old masters that do not know about identity metadata
+	name := user.GetName()
+	uid := user.GetUID()
+
+	// assume not identity metadata by default
+	identityMetadata := ""
+	// check if we need have optional identity metadata (for storing a reference to group information)
+	if userIdentityMetadata, ok := user.(authapi.UserIdentityMetadata); ok {
+		identityMetadata = userIdentityMetadata.GetIdentityMetadataName()
+		// TODO maybe do this once we no longer have mixed master scenarios to worry about
+		// unset name and UID since identity metadata is authoritative
+		// name = ""
+		// uid = ""
+	}
+
+	return false, a.put(w, name, uid, identityMetadata, expires)
 }
 
 func (a *Authenticator) InvalidateAuthentication(w http.ResponseWriter, req *http.Request) error {
 	// zero out all fields
-	return a.put(w, "", "", 0)
+	return a.put(w, "", "", "", 0)
 }
 
-func (a *Authenticator) put(w http.ResponseWriter, name, uid string, expires int64) error {
+func (a *Authenticator) put(w http.ResponseWriter, name, uid, identityMetadata string, expires int64) error {
 	values := Values{}
 
 	values[userNameKey] = name
@@ -102,6 +143,8 @@ func (a *Authenticator) put(w http.ResponseWriter, name, uid string, expires int
 	} else {
 		values[expiresKey] = strconv.FormatInt(expires, 10)
 	}
+
+	values[identityMetadataNameKey] = identityMetadata
 
 	return a.store.Put(w, values)
 }
