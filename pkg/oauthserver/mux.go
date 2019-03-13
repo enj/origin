0df5dd959367e4dac39d525a31c0ff580893ba6a
Monis Khan <mkhan@redhat.com>
2019-03-12 22:16:37 -0400

    Fix OAuth endpoints interface
    Signed-off-by: Monis Khan <mkhan@redhat.com>

--- pkg/oauthserver/mux.go
+++ pkg/oauthserver/mux.go
@@ -15,6 +15,13 @@ type Mux interface {
 	HandleFunc(pattern string, handler func(http.ResponseWriter, *http.Request))
 }
 
+type Endpoints interface {
+	// Install registers one or more http.Handlers into the given mux.
+	// It is expected that the provided prefix will serve all operations.
+	// prefix MUST NOT end in a slash.
+	Install(mux Mux, prefix string)
+}
+
 func NewMuxWithStandardHeaders() Mux {
 	return &muxWrapper{
 		mux: http.NewServeMux(),
