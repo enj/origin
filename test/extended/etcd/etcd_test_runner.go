b3e8374a5cd286059c9a4f67e2fb623e65cc74fe
Monis Khan <mkhan@redhat.com>
2019-07-18 17:21:42 -0400

    wip002
    Signed-off-by: Monis Khan <mkhan@redhat.com>

--- test/extended/etcd/etcd_test_runner.go
+++ test/extended/etcd/etcd_test_runner.go
@@ -7,8 +7,10 @@ import (
 	g "github.com/onsi/ginkgo"
 	o "github.com/onsi/gomega"
 
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/apimachinery/pkg/util/wait"
 	restclient "k8s.io/client-go/rest"
 
 	routev1 "github.com/openshift/api/route/v1"
@@ -20,14 +22,14 @@ var _ = g.Describe("API data in etcd", func() {
 
 	oc := exutil.NewCLI("etcd-storage-path", exutil.KubeConfigPath())
 
-	g.It("should be stored at the correct location and version for all resources", func() {
+	_ = g.It("should be stored at the correct location and version for all resources", func() {
 		const (
-			name      = "etcd"
-			namespace = "openshift-etcd"
-			port      = 2379
+			name            = "etcd"
+			etcdNamespace   = "openshift-etcd"
+			configNamespace = "openshift-config"
 		)
 
-		routes := oc.AdminRouteClient().RouteV1().Routes(namespace)
+		routes := oc.AdminRouteClient().RouteV1().Routes(etcdNamespace)
 		defer func() {
 			o.Expect(routes.Delete(name, nil)).NotTo(o.HaveOccurred())
 		}()
@@ -35,7 +37,7 @@ var _ = g.Describe("API data in etcd", func() {
 		_, err := routes.Create(&routev1.Route{
 			ObjectMeta: metav1.ObjectMeta{
 				Name:      name,
-				Namespace: namespace,
+				Namespace: etcdNamespace,
 			},
 			Spec: routev1.RouteSpec{
 				To: routev1.RouteTargetReference{
@@ -43,7 +45,7 @@ var _ = g.Describe("API data in etcd", func() {
 					Name: name,
 				},
 				Port: &routev1.RoutePort{
-					TargetPort: intstr.FromInt(port),
+					TargetPort: intstr.FromInt(2379),
 				},
 				TLS: &routev1.TLSConfig{
 					Termination: routev1.TLSTerminationPassthrough,
@@ -52,26 +54,74 @@ var _ = g.Describe("API data in etcd", func() {
 		})
 		o.Expect(err).NotTo(o.HaveOccurred())
 
-		// get route host name
+		var etcdHost string
+		err = wait.PollImmediate(time.Second, wait.ForeverTestTimeout, func() (done bool, err error) {
+			route, err := routes.Get(name, metav1.GetOptions{})
+			if err != nil {
+				return false, err
+			}
+			etcdHost = getHost(route)
+			return len(etcdHost) > 0, nil
+		})
+		o.Expect(err).NotTo(o.HaveOccurred())
+
+		coreV1 := oc.AdminKubeClient().CoreV1()
+		etcdConfigMap, err := coreV1.ConfigMaps(configNamespace).Get("etcd-ca-bundle", metav1.GetOptions{})
+		o.Expect(err).NotTo(o.HaveOccurred())
+		etcdSecret, err := coreV1.Secrets(configNamespace).Get("etcd-client", metav1.GetOptions{})
+		o.Expect(err).NotTo(o.HaveOccurred())
+		etcdEndpoint, err := coreV1.Endpoints(etcdNamespace).Get("host-etcd", metav1.GetOptions{})
+		o.Expect(err).NotTo(o.HaveOccurred())
+		infrastructure, err := oc.AdminConfigClient().ConfigV1().Infrastructures().Get("cluster", metav1.GetOptions{})
+		o.Expect(err).NotTo(o.HaveOccurred())
 
 		tlsConfig, err := restclient.TLSConfigFor(&restclient.Config{
 			TLSClientConfig: restclient.TLSClientConfig{
-				ServerName: "",
-				CertData:   nil,
-				KeyData:    nil,
-				CAData:     nil,
+				ServerName: getHostName(etcdEndpoint) + "." + infrastructure.Status.EtcdDiscoveryDomain,
+				CertData:   etcdSecret.Data[corev1.TLSCertKey],
+				KeyData:    etcdSecret.Data[corev1.TLSPrivateKeyKey],
+				CAData:     []byte(etcdConfigMap.Data["ca-bundle.crt"]),
 			},
 		})
 		o.Expect(err).NotTo(o.HaveOccurred())
 
-		etcdConfig := clientv3.Config{
-			Endpoints:   []string{"TODO"},
+		etcdClient3, err := clientv3.New(clientv3.Config{
+			Endpoints:   []string{"https://" + etcdHost},
 			DialTimeout: 30 * time.Second,
 			TLS:         tlsConfig,
-		}
-		etcdClient3, err := clientv3.New(etcdConfig)
+		})
 		o.Expect(err).NotTo(o.HaveOccurred())
 
 		testEtcd3StoragePath(g.GinkgoT(), oc.AdminConfig(), etcdClient3.KV)
 	})
 })
+
+func getHost(route *routev1.Route) string {
+	for _, ingress := range route.Status.Ingress {
+		if !isIngressAdmitted(ingress) {
+			continue
+		}
+		return ingress.Host
+	}
+	return ""
+}
+
+func isIngressAdmitted(ingress routev1.RouteIngress) bool {
+	for _, condition := range ingress.Conditions {
+		if condition.Type == routev1.RouteAdmitted && condition.Status == corev1.ConditionTrue {
+			return true
+		}
+	}
+	return false
+}
+
+func getHostName(ep *corev1.Endpoints) string {
+	for _, s := range ep.Subsets {
+		for _, a := range s.Addresses {
+			if len(a.Hostname) > 0 {
+				return a.Hostname
+			}
+		}
+	}
+	return "not a valid hostname!"
+}
